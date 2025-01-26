ab4bd9a99860c61b8d5e640a9fb8076ac559a110
Dylan Fitch <kkarl30@gmail.com>
2025-01-26 14:02:17 -0500

    getting weather API response

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import "./styles.css";
 
+import {fetchWeather} from './modules/weatherAPI.js';
+
--- src/modules/weatherAPI.js
+++ src/modules/weatherAPI.js
@@ -1,3 +1,25 @@
 
 
+const KEY = 'D7HZ9NJBPMCSGR9BYN6XTRJGP';
+const URL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline';
 
+
+export async function fetchWeather(location){
+
+    fetch(`${URL}/${location}?key=${KEY}`, {mode: 'cors'})
+        .then(response => {
+            console.log(response);
+            if (!response.ok){
+                console.log(`Request failed with status: ${response.status} (${response.statusText})`)
+                throw new Error(`Failed to fetch weather data`);
+            } 
+
+            return response.json();
+            
+        })
+        .then(response => console.log(response))
+        .catch(err => {
+            console.log(err);
+        })
+
+}
